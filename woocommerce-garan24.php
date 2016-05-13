ddbe25a91df187a6fccfce839484a850a16c376e
vbushuev <v.bushuev@gmail.com>
2016-05-13 07:29:49 +0300

    no blank lines

--- woocommerce-garan24.php
+++ woocommerce-garan24.php
@@ -4,44 +4,32 @@ class WC_Gateway_garan24 extends WC_Payment_Gateway {
 
 	// Setup our Gateway's id, description and other values
 	function __construct() {
-
 		// The global ID for this Payment method
 		$this->id = "garan24";
-
 		// The Title shown on the top of the Payment Gateways Page next to all the other Payment Gateways
 		$this->method_title = __( "Garan24 pay", 'garan24' );
-
 		// The description for this Payment Gateway, shown on the actual Payment options page on the backend
 		$this->method_description = __( "Garan24 Payment Gateway Plug-in for WooCommerce", 'garan24' );
-
 		// The title to be used for the vertical tabs that can be ordered top to bottom
 		$this->title = __( "Garan24", 'garan24' );
-
 		// If you want to show an image next to the gateway's name on the frontend, enter a URL to an image.
 		$this->icon = null;
-
 		// Bool. Can be set to true if you want payment fields to show on the checkout
 		// if doing a direct integration, which we are doing in this case
 		$this->has_fields = false;
-
 		// Supports the default credit card form
 		//$this->supports = array( 'default_credit_card_form' );
-
 		// This basically defines your settings which are then loaded with init_settings()
 		$this->init_form_fields();
-
 		// After init_settings() is called, you can get the settings and load them into variables, e.g:
 		// $this->title = $this->get_option( 'title' );
 		$this->init_settings();
-
 		// Turn these settings into variables we can use
 		foreach ( $this->settings as $setting_key => $value ) {
 			$this->$setting_key = $value;
 		}
-
 		// Lets check for SSL
 		//add_action( 'admin_notices', array( $this,	'do_ssl_check' ) );
-
 		// Save settings
 		if ( is_admin() ) {
 			// Versions over 2.0
@@ -52,7 +40,6 @@ class WC_Gateway_garan24 extends WC_Payment_Gateway {
 		}
 		add_action('woocommerce_receipt_garan24', array(&$this, 'receipt_page'));
 	} // End __construct()
-
 	// Build the administration fields for this specific Gateway
 	public function init_form_fields() {
 		$this->form_fields = array(
